ac5447e46294e08caeaedabe77d05d384eb73ee7
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-04-04 01:04:34 +0100

    better exception handling.

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,5 +1,6 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.FailedUpdatingStatusException
 import io.pleo.antaeus.core.exceptions.NoPendingInvoiceException
 import io.pleo.antaeus.core.exceptions.UnableToChargeInvoiceException
 import io.pleo.antaeus.core.external.PaymentProvider
@@ -12,9 +13,9 @@ class BillingService(
     private val invoiceService: InvoiceService
 ) {
     private val logger = KotlinLogging.logger {}
-    private var successfulList: MutableList<Invoice> = mutableListOf()
+    //private var successfulList: MutableList<Invoice> = mutableListOf()
     private var failedList: MutableList<Invoice> = mutableListOf()
-    fun processPendingInvoices(): List<Invoice> {
+    fun processAllPendingInvoices(): List<Invoice> {
         // val currentDate = LocalDate.now()
         // val firstDayOfMonth = LocalDate.of(currentDate.year, currentDate.month, 1)
         val invoices = invoiceService.fetchAllPending()
@@ -22,31 +23,29 @@ class BillingService(
             throw NoPendingInvoiceException()
         }
         for (invoice in invoices) {
-            val updatedInvoice = this.chargeInvoice(invoice)
-            if (updatedInvoice == null) {
+            try {
+                chargeInvoice(invoice)
+            } catch (e: UnableToChargeInvoiceException) {
                 failedList.add(invoice)
-            } else {
-                logger.info("Updated invoice => $updatedInvoice")
-                successfulList.add(updatedInvoice)
+            } catch (e: FailedUpdatingStatusException) {
+                failedList.add(invoice)
             }
         }
+
         return failedList
     }
-    private fun chargeInvoice(inv: Invoice): Invoice? {
+    private fun chargeInvoice(inv: Invoice): Invoice {
         val successfullyCharged = paymentProvider.charge(inv)
         logger.info("Invoice ${inv.id} charged? $successfullyCharged")
         if (!successfullyCharged) {
             logger.info("Failed at invoice ${inv.id}")
-            // throw UnableToChargeInvoiceException(inv.id)
-            return null
-
+            throw UnableToChargeInvoiceException(inv)
         }
-        val updatedInvoice = invoiceService.updateInvoiceStatus(inv.id)
-        if (updatedInvoice.status != InvoiceStatus.PAID) {
-            // Something went wrong ?
-            logger.info("Failed: ${updatedInvoice.status} is still pending for invoice ${inv.id}")
-            return null
+        try {
+            return invoiceService.updateInvoiceStatus(inv.id, InvoiceStatus.PAID)
+        } catch (e: FailedUpdatingStatusException) {
+            paymentProvider.cancelCharge(inv)
+            throw e
         }
-        return updatedInvoice
     }
 }
