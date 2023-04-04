7d42f7413a4158f27b83c5dda9c00437c1c7cdec
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-04-04 18:46:45 +0100

    first of the month check

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -3,21 +3,28 @@ package io.pleo.antaeus.core.services
 import io.pleo.antaeus.core.exceptions.FailedUpdatingStatusException
 import io.pleo.antaeus.core.exceptions.NoPendingInvoiceException
 import io.pleo.antaeus.core.exceptions.UnableToChargeInvoiceException
+import io.pleo.antaeus.core.exceptions.WrongDateToChargeException
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import mu.KotlinLogging
+import java.time.LocalDate
 
 class BillingService(
     private val paymentProvider: PaymentProvider,
     private val invoiceService: InvoiceService
 ) {
     private val logger = KotlinLogging.logger {}
-    //private var successfulList: MutableList<Invoice> = mutableListOf()
     private var failedList: MutableList<Invoice> = mutableListOf()
     fun processAllPendingInvoices(): List<Invoice> {
+        // This code ensures that the charging only occurs at the first of the month
+        // However, I will leave it commented for testing purposes: it makes it easier to test the functionality.
+
         // val currentDate = LocalDate.now()
-        // val firstDayOfMonth = LocalDate.of(currentDate.year, currentDate.month, 1)
+        // if (currentDate.dayOfMonth != 1) {
+        //     throw WrongDateToChargeException()
+        // }
+
         val invoices = invoiceService.fetchAllPending()
         if (invoices.isEmpty()) {
             throw NoPendingInvoiceException()
@@ -31,7 +38,6 @@ class BillingService(
                 failedList.add(invoice)
             }
         }
-
         return failedList
     }
     private fun chargeInvoice(inv: Invoice): Invoice {
