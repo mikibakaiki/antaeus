5435553b1457ea497cd733ab05b5316dba203d6a
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-04-05 00:00:50 +0100

    aesthetics in exception message

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/exceptions/UnableToChargeInvoiceException.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/exceptions/UnableToChargeInvoiceException.kt
@@ -2,4 +2,4 @@ package io.pleo.antaeus.core.exceptions
 
 import io.pleo.antaeus.models.Invoice
 
-class UnableToChargeInvoiceException(invoice: Invoice, reason: String?) : Exception("Unable to charge Invoice $invoice\n Reason: ${reason?:"Unknown"}")
+class UnableToChargeInvoiceException(invoice: Invoice, reason: String?) : Exception("Unable to charge $invoice\n Reason: ${reason?:"Unknown"}")
