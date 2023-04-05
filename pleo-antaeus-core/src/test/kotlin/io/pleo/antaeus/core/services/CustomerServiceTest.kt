4c2b53fb924fc83b9623c32c57c431660dad3445
mikibakaiki <joaomiguelcampos6@gmail.com>
2023-04-05 18:45:11 +0100

    customer service tests

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/CustomerServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/CustomerServiceTest.kt
@@ -4,12 +4,20 @@ import io.mockk.every
 import io.mockk.mockk
 import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
+import kotlin.random.Random
 
 class CustomerServiceTest {
+    private val customer1 = Customer(1, Currency.DKK)
+    private val customer2 = Customer(2, Currency.EUR)
+
     private val dal = mockk<AntaeusDal> {
         every { fetchCustomer(404) } returns null
+        every { fetchCustomer(1) } returns customer1
+        every { fetchCustomers() } returns listOf(customer1, customer2)
     }
 
     private val customerService = CustomerService(dal = dal)
@@ -19,5 +27,18 @@ class CustomerServiceTest {
         assertThrows<CustomerNotFoundException> {
             customerService.fetch(404)
         }
+    }
+    @Test
+    fun `should fetch a single customer`() {
+        val customer = customerService.fetch(1)
+        assert(customer == customer1)
+    }
+
+    @Test
+    fun `should fetch all customers`() {
+        val customers = customerService.fetchAll()
+        assert(customers.size == 2)
+        assert(customers[0] == customer1)
+        assert(customers[1] == customer2)
     }
 }
